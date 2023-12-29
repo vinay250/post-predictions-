af653fd9038ac08afe452e0df9b76cbf2bc53973
“vinayaka <vinayakavirat008@gmail.com>
2023-12-29 20:20:24 +0530

    updated

--- source/postpredictions/components/data_ingestion.py
+++ source/postpredictions/components/data_ingestion.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from source.postpredictions.logger import logging
-from source.postpredictions.exception import CustomException
+
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -48,11 +48,4 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
-    data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
-
-    modeltrainer=ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
-
-
-
+    
