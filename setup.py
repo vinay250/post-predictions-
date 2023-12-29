56d1c188beeda9a6bef89d724376f4e5c950554d
“vinayaka <vinayakavirat008@gmail.com>
2023-12-29 18:05:00 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='postpredictions',
+    version='0.0.1',
+    author='vinay',
+    author_email='vinayakavirat008@gmail.com',
+    install_requires=["pandas","numpy","scikit-learn"],
+    packages=find_packages()
+)
