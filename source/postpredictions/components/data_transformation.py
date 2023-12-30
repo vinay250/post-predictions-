f5d08ca1dd03fa523ab9abe4b216cb52432ddb13
“vinayaka <vinayakavirat008@gmail.com>
2023-12-30 22:54:59 +0530

    updated

--- source/postpredictions/components/data_transformation.py
+++ source/postpredictions/components/data_transformation.py
@@ -26,7 +26,7 @@ class DataTransformation:
         # ...
 
         # Split the data into features (X) and target variable (y)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
         return X_train, X_test, y_train, y_test
     def __init__(self):
