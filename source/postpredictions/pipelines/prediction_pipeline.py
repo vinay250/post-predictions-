2685e41deaaebf42836a970f4d38b1e7c2291538
“vinayaka <vinayakavirat008@gmail.com>
2024-01-02 22:11:54 +0530

    updated

--- source/postpredictions/pipelines/prediction_pipeline.py
+++ source/postpredictions/pipelines/prediction_pipeline.py
@@ -8,17 +8,22 @@ from sklearn.model_selection import train_test_split
 from source.postpredictions.exception import CustomException
 from source.postpredictions.logger import logging
 from source.postpredictions.utils.utils import load_object, save_object
+from dataclasses import dataclass
 
+@dataclass
+class PredictPipline:
+    Pipline_model_file = os.path.join('artifacts','Pipline.pkl')
+    
 class PredictPipeline:
     def __init__(self):
         self.model_loaded = False  # Assume the model is not loaded initially
-        self.pipeline = None
+        self.Pipline = None
 
     def load_model(self, model_path):
         try:
             # Load the pipeline from files
-            preprocessor_path = os.path.join(model_path, "preprocessor.pkl")
-            self.preprocessor = load_object(pipeline_path)
+            Pipline_path = os.path.join(model_path, "Pipline.pkl")
+            self.Pipline = load_object(pipeline_path)
 
             self.model_loaded = True  # Set the flag to indicate that the model is loaded
             logging.info('Model loaded successfully')
@@ -37,7 +42,7 @@ class PredictPipeline:
 
             # Make prediction
             try:
-                pred = self.preprocessor.predict([text])
+                pred = self.Pipline.predict([text])
             except Exception as e:
                 # Handle the case where the prediction itself causes an error
                 raise CustomException(f"Error during prediction: {e}")
